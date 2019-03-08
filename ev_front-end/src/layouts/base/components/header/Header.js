97e6942b07fd260b1d2e884d1b0587fabfbcfa53
Vladislav <zvvlad2@gmail.com>
2019-03-08 22:22:32 +0600

    start working with html and css

---
+++ ev_front-end/src/layouts/base/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style.css';
+import logo from "./images/logo_white.png"
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className='header'>
+        <div className='header__content'>
+            <a className='header__logo' href='/'>
+                <img src={logo} alt={""}>
+                </img>
+          </a>
+        </div>
+      </header>
+    );
+  };
+};
