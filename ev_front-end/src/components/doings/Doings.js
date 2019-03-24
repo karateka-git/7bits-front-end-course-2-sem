d156a5d5e6cee07ab0653f341f822cc6b1c41fb0
Vladislav <zvvlad2@gmail.com>
2019-03-24 20:46:46 +0600

    add handleClickOnButton - button click processing

--- ev_front-end/src/components/doings/Doings.js
+++ ev_front-end/src/components/doings/Doings.js
@@ -7,12 +7,12 @@ import Button from "./button/Button";
 export default class Doings extends React.Component {
   render() {
     return (
-        <div className={`main__task main__task_${this.props.page} task-id-${this.props.taskNumber}`}>
-          <Button name={"done"}/>
+        <div className={`main__task main__task_${this.props.page}`}>
+          <Button name={"done"} id={this.props.taskNumber}/>
           {/*<button className='button__b-done button main__button_task'/>*/}
           <h1 className='task__description'>{this.props.description}</h1>
-          <Button name={"pen"}/>
-          <Button name={"delete"}/>
+          <Button name={"pen"} id={this.props.taskNumber}/>
+          <Button name={"delete"} id={this.props.taskNumber}/>
           {/*<button className='button__b-pen button main__button_task'/>*/}
           {/*<button className='button__b-delete button main__button_task'/>*/}
         </div>
--- ev_front-end/src/components/doings/button/Button.js
+++ ev_front-end/src/components/doings/button/Button.js
@@ -4,17 +4,30 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 export default class Button extends React.Component {
+    handleClickOnButton;
+    constructor(props) {
+        super(props);
+        this.handleClickOnButton = this.handleClickOnButton.bind(this);
+    }
+
+    static handleClickOnButton(id) {
+        alert(id);
+    }
+
     render() {
         return (
-            <button className={`button__b-${this.props.name} button main__button_task`}/>
+            <button onClick={this.handleClickOnButton.bind(this, this.props.id)}
+                    className={`button__b-${this.props.name} button main__button_task`}/>
         );
     };
 };
 
 Button.propTypes = {
-    name: PropTypes.string
+    name: PropTypes.string,
+    id: PropTypes.string
 };
 
 Button.defaultProps = {
-    name: ''
+    name: '',
+    id: ''
 };
