3eb2c5ed854299f58d39a441d8ca1546cdf8f54c
Vladislav <zvvlad2@gmail.com>
2019-03-24 17:36:54 +0600

    Revert "beginning of practice"
    This reverts commit c9157af3df6b8ec063abbdc6fbea5f907d85c4bb.

--- ev_front-end/src/components/doings/Doings.js
+++ ev_front-end/src/components/doings/Doings.js
@@ -7,11 +7,14 @@ import Button from "./button/Button";
 export default class Doings extends React.Component {
   render() {
     return (
-        <div className={`main__task main__task_${this.props.page} task-id-${this.props.taskNumber}`}>
+        <div className={`main__task main__task_${this.props.page}`}>
           <Button name={"done"}/>
+          {/*<button className='button__b-done button main__button_task'/>*/}
           <h1 className='task__description'>{this.props.description}</h1>
           <Button name={"pen"}/>
           <Button name={"delete"}/>
+          {/*<button className='button__b-pen button main__button_task'/>*/}
+          {/*<button className='button__b-delete button main__button_task'/>*/}
         </div>
     );
   };
