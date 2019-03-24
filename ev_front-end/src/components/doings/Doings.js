c9157af3df6b8ec063abbdc6fbea5f907d85c4bb
Vladislav <zvvlad2@gmail.com>
2019-03-24 17:33:50 +0600

    starting practice

--- ev_front-end/src/components/doings/Doings.js
+++ ev_front-end/src/components/doings/Doings.js
@@ -7,14 +7,11 @@ import Button from "./button/Button";
 export default class Doings extends React.Component {
   render() {
     return (
-        <div className={`main__task main__task_${this.props.page}`}>
+        <div className={`main__task main__task_${this.props.page} task-id-${this.props.taskNumber}`}>
           <Button name={"done"}/>
-          {/*<button className='button__b-done button main__button_task'/>*/}
           <h1 className='task__description'>{this.props.description}</h1>
           <Button name={"pen"}/>
           <Button name={"delete"}/>
-          {/*<button className='button__b-pen button main__button_task'/>*/}
-          {/*<button className='button__b-delete button main__button_task'/>*/}
         </div>
     );
   };
