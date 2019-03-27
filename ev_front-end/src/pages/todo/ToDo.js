3fac1cc77c1c8a6ba1249d91fffa02473cb7c7fe
Vladislav <zvvlad2@gmail.com>
2019-03-27 17:46:46 +0600

    need to make a good component 'button'

--- ev_front-end/src/pages/todo/ToDo.js
+++ ev_front-end/src/pages/todo/ToDo.js
@@ -5,20 +5,44 @@ import Doings from '../../components/doings/Doings';
 import list from './list';
 
 import './style.css';
+import FormField from "../../components/formField/FormField";
 
 export default class ToDo extends React.Component {
-  renderList = () => {
-    return list.data.map((item, index) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      itemList: []
+    };
+  }
+  renderList = (itemList) => {
+    const items = itemList.map((item, index) => {
       return (
         <Doings key={index} taskNumber={item.id} description={item.description} page={'todo'}/>
       );
     });
   };
 
+  onChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  };
+
   render() {
     return (
       <React.Fragment>
-        {this.renderList()}
+        <form className='todo-form'>
+          <FormField
+              className={'form__field'}
+              placeholder={'Type you new task'}
+              onChange={this.onChange}
+          />
+          <button className={'button form_button'} type='submit' >
+            create
+          </button>
+        </form>
+        {this.renderList(this.state.itemList)}
       </React.Fragment>
     );
   };
